47cc436aab80e8c76265b6ba2c1c896affc003c1
Stephen Sugden <glurgle@gmail.com>
2013-03-12 23:29:43 -0700

    Make POST /still/x/sensor/y take data from request body

--- server/distillery/sensorPOST.py
+++ server/distillery/sensorPOST.py
@@ -2,8 +2,9 @@ from flask import jsonify, request
 from distillery import app
 import database as db
 
-@app.route("/still/<int:still_id>/sensor/<int:sensor_id>/value/<int:sensor_value>", methods=['POST'])
-def sensor(still_id, sensor_id, sensor_value):
+@app.route("/still/<int:still_id>/sensor/<int:sensor_id>",
+           methods=['POST'])
+def sensor(still_id, sensor_id):
     return jsonify(db.add_sensor_data(still_id,
                                       sensor_id,
-                                      sensor_value))
+                                      request.data))
