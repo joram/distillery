b34a7147a92ee1bb260a8aca65f789e68913ecbf
Stephen Sugden <glurgle@gmail.com>
2013-03-12 21:41:10 -0700

    Use jsonify

--- server/distillery/sensorPOST.py
+++ server/distillery/sensorPOST.py
@@ -1,9 +1,9 @@
-from flask import request
+from flask import jsonify, request
 from distillery import app
 import databaseHelper as dbHelper
 
 @app.route("/still/<int:still_id>/sensor/<int:sensor_id>/value/<int:sensor_value>", methods=['POST'])
 def sensor(still_id, sensor_id, sensor_value):
-	dbHelper.addSensorData(still_id, sensor_id, sensor_value)
-	return  "SUCCESS"
-
+    return jsonify(dbHelper.addSensorData(still_id,
+                                          sensor_id,
+                                          sensor_value))
