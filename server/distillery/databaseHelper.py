2cc7416b635d5dee0f79e0647a476854135fadf5
Stephen Sugden <glurgle@gmail.com>
2013-03-12 21:49:52 -0700

    fix style in databaseHelper

--- server/distillery/databaseHelper.py
+++ server/distillery/databaseHelper.py
@@ -1,18 +1,20 @@
 import sqlite3
 import datetime
+
 _db_filename = "database.sqlite3"
+
 
 def dbCursor():
     dbConnection = sqlite3.connect(_db_filename)
     dbCursor = dbConnection.cursor()
-    return dbCursor    
+    return dbCursor
 
 
 def checkStill(still_id):
     cursor = dbCursor()
     cursor.execute("create table if not exists stills(id INT)")
     cursor.execute('SELECT * FROM stills WHERE id==%s' % still_id)
-    if len(cursor.fetchall())<=0:
+    if len(cursor.fetchall()) <= 0:
         cursor.execute("INSERT INTO stills(id) values (%s)" % still_id)
 
 
@@ -20,15 +22,26 @@ def checkSensor(still_id, sensor_id):
     checkStill(still_id)
     cursor = dbCursor()
     cursor.execute("create table if not exists sensors(still INT, id INT)")
-    cursor.execute("create table if not exists sensorData(still INT, sensor INT, time DATETIME, value TEXT(32))")
-    cursor.execute('SELECT * FROM sensors WHERE still==%s AND id==%s' % (still_id,sensor_id))
-    if len(cursor.fetchall() )<=0:
-        cursor.execute("INSERT INTO sensors(still, id) values (%s, %s)" % (still_id, sensor_id))
-    
-    
+    cursor.execute("""
+        create table if not exists sensorData (
+            still INT,
+            sensor INT,
+            time DATETIME,
+            value TEXT(32)
+        )
+    """)
+    cursor.execute('SELECT * FROM sensors WHERE still = ? AND id = ?',
+                   (still_id, sensor_id))
+    if len(cursor.fetchall()) <= 0:
+        cursor.execute("INSERT INTO sensors(still, id) values (?, ?)",
+                       (still_id, sensor_id))
+
+
 def addSensorData(still_id, sensor_id, sensor_value):
     checkSensor(still_id, sensor_id)
     cursor = dbCursor()
     dtime = datetime.datetime.now()
-    cursor.execute("INSERT INTO sensorData(still, sensor, time, value) values (?,?,?,?)", (still_id, sensor_id, dtime, sensor_value))
-
+    cursor.execute(
+        "INSERT INTO sensorData(still, sensor, time, value) values (?,?,?,?)",
+        (still_id, sensor_id, dtime, sensor_value)
+    )
