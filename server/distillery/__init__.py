9a4f04adccdb9ac46638c21ece91e19f67074e08
Stephen Sugden <glurgle@gmail.com>
2013-03-12 23:27:56 -0700

    whitespace and unused imports

--- server/distillery/__init__.py
+++ server/distillery/__init__.py
@@ -1,11 +1,12 @@
-import os
-import sqlite3
 from flask import Flask
+
 app = Flask(__name__)
+
 
 @app.route("/favicon.ico")
 def favicon():
     return app.send_static_file("favicon.ico")
+
 
 import distillery.sensorGET
 import distillery.sensorPOST
