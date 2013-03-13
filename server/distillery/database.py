b8e5419825990bb57709db85627591673b77cefb
Stephen Sugden <glurgle@gmail.com>
2013-03-12 23:32:00 -0700

    rewrite database module, make stuff work

--- server/distillery/database.py
+++ server/distillery/database.py
@@ -1,28 +1,54 @@
+from flask import g
 import sqlite3
-import datetime
+from datetime import datetime, timedelta
 
-_db_filename = "database.sqlite3"
+from distillery import app
 
 
-def db_cursor():
-    db_connection = sqlite3.connect(_db_filename)
-    db_cursor = db_connection.cursor()
-    return db_cursor
+def _connect():
+    conn = sqlite3.connect("database.sqlite3")
+    conn.row_factory = sqlite3.Row
+    return conn
+
+
+def get_connection():
+    """
+    Return the request-context database connection or get a new one.
+    """
+    return getattr(g, 'db', False) or _connect()
+
+
+@app.before_request
+def connect_database():
+    g.db = get_connection()
+
+
+@app.teardown_request
+def close_database(exc):
+    if hasattr(g, 'db'):
+        g.db.close()
+
+
+def execute(*args):
+    return get_connection().execute(*args)
+
+
+def commit():
+    return get_connection().commit()
 
 
 def check_still(still_id):
-    cursor = db_cursor()
-    cursor.execute("create table if not exists stills(id INT)")
-    cursor.execute('SELECT * FROM stills WHERE id==%s' % still_id)
-    if len(cursor.fetchall()) <= 0:
-        cursor.execute("INSERT INTO stills(id) values (%s)" % still_id)
+    execute("create table if not exists stills(id INT)")
+    cursor = execute('SELECT * FROM stills WHERE id=?', (still_id,))
+    if not len(cursor.fetchall()):
+        execute("INSERT INTO stills (id) values (?)", (still_id,))
+        commit()
 
 
 def check_sensor(still_id, sensor_id):
     check_still(still_id)
-    cursor = db_cursor()
-    cursor.execute("create table if not exists sensors(still INT, id INT)")
-    cursor.execute("""
+    execute("create table if not exists sensors(still INT, id INT)")
+    execute("""
         create table if not exists sensor_data (
             still INT,
             sensor INT,
@@ -30,18 +56,37 @@ def check_sensor(still_id, sensor_id):
             value TEXT(32)
         )
     """)
-    cursor.execute('SELECT * FROM sensors WHERE still = ? AND id = ?',
-                   (still_id, sensor_id))
-    if len(cursor.fetchall()) <= 0:
-        cursor.execute("INSERT INTO sensors(still, id) values (?, ?)",
-                       (still_id, sensor_id))
+    res = execute('SELECT * FROM sensors WHERE still = ? AND id = ?',
+                  (still_id, sensor_id))
+    if not len(res.fetchall()):
+        execute("INSERT INTO sensors(still, id) values (?, ?)",
+                (still_id, sensor_id))
+        commit()
 
 
 def add_sensor_data(still_id, sensor_id, sensor_value):
     check_sensor(still_id, sensor_id)
-    cursor = db_cursor()
-    dtime = datetime.datetime.now()
-    cursor.execute(
-        "INSERT INTO sensor_data(still, sensor, time, value) values (?,?,?,?)",
-        (still_id, sensor_id, dtime, sensor_value)
-    )
+    dtime = datetime.now()
+    sql = """INSERT INTO sensor_data (still, sensor, time, value)
+             values (?,?,?,?)"""
+    execute(sql, (still_id, sensor_id, dtime, sensor_value))
+    commit()
+    return {'still':  still_id,
+            'sensor': sensor_id,
+            'time':   dtime.isoformat(),
+            'value':  sensor_value}
+
+
+def get_sensor_history(still, sensor, seconds_history):
+    check_sensor(still, sensor)
+    sql = """SELECT time, value FROM sensor_data
+             WHERE still = ? AND sensor = ? AND time >= ?
+             ORDER BY time DESC"""
+    time = datetime.now() - timedelta(seconds=seconds_history)
+    rows = execute(sql, (still, sensor, time)).fetchall()
+
+    return [dict(row) for row in rows]
+
+
+def get_sensor_list(still):
+    return execute("SELECT id FROM sensors WHERE still=?", (still,))
--- server/distillery/sensorGET.py
+++ server/distillery/sensorGET.py
@@ -1,22 +1,20 @@
-from flask import jsonify, url_for
+from flask import jsonify, request, url_for
 from distillery import app
 import database as db
 
 
-def get_sensor_data(still_id, sensor_id, seconds_history):
-    return -1
+@app.route("/still/<int:still_id>/sensor/<int:sensor_id>",
+           methods=['GET'])
+def get_sensor(still_id, sensor_id):
+    try:
+        seconds_history = int(request.args['seconds_history'])
+    except (KeyError, TypeError):
+        seconds_history = 1000
 
-
-@app.route("/still/<int:still_id>/sensor/<int:sensor_id>/history/<int:seconds_history>", methods=['GET'])
-@app.route("/still/<int:still_id>/sensor/<int:sensor_id>", methods=['GET'])
-def sensor(still_id, sensor_id, seconds_history=100):
-    #sensor_val = get_sensor_data(still_id, sensor_id, seconds_history)
-
-    returned_data = {}
-    returned_data["sensor1"] = [("time1", "value1"), ("time2", "value2")]
-    returned_data["sensor2"] = [("time3", "value3"), ("time4", "value4")]
-
-    return jsonify(returned_data)
+    history = db.get_sensor_history(still_id,
+                                    sensor_id,
+                                    seconds_history)
+    return jsonify(history=history)
 
 
 @app.route("/still/<int:still_id>/sensors", methods=['GET'])
@@ -24,15 +22,14 @@ def sensor_list(still_id):
     """ Return array of links to sensors """
     sensors = []
 
-    for sensor_id in [0, 1, 2]:
+    for row in db.get_sensor_list(still_id):
         sensors.append(
-            url_for('sensor', still_id=still_id, sensor_id=sensor_id))
+            url_for('get_sensor', still_id=still_id, sensor_id=row[0]))
 
-    return jsonify(sensors)
+    return jsonify(sensors=sensors)
 
 
 @app.route("/debug", methods=['GET'])
 def debug():
-    cur = db.db_cursor()
-    cur.execute("SELECT * FROM sensor_data")
-    return jsonify(cur.fetchall())
+    rows = [dict(row) for row in db.execute("SELECT * FROM sensor_data")]
+    return jsonify(rows=rows)
