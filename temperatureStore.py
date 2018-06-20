30b7d2b31ddef77bba43ed881e85fbabf589f7dd
John Oram <john.c.oram@gmail.com>
2018-06-20 15:06:55 -0700

    Update temperatureStore.py

--- temperatureStore.py
+++ temperatureStore.py
@@ -15,6 +15,16 @@ class TemperatureStore(TimeSeriesStore):
     def stop(self):
         self.RUNNING = False
 
+    def poll_temp(channel=0, sleep_s=1):
+        while self.RUNNING:
+            temperature = ads1256.read_channel(channel)
+            channel_name = "probe "+str(channel)
+            self.add_temp(channel_name, temperature)
+            time.sleep(sleep_s)
+            if not SELF.RUNNING:
+                break
+        print "done polling"
+    
     def _init_board(self):
         print "initing board..."
         print "inited board..."
