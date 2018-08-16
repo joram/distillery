16888901c9bf90f9e41e088918e233167df2fb5d
John <john.c.oram@gmail.com>
2018-08-16 02:21:01 +0000

    starting on the motor controller

---
+++ README.md
@@ -0,0 +1,36 @@
+# hardward
+[Motor Controller HAT](https://learn.adafruit.com/adafruit-dc-and-stepper-motor-hat-for-raspberry-pi/using-dc-motors)
+[Analog to Digital HAT](https://www.waveshare.com/wiki/High-Precision_AD/DA_Board)
+
+
+# setup
+
+## install motor hat library
+```
+git clone https://github.com/adafruit/Adafruit-Motor-HAT-Python-Library.git
+cd Adafruit-Motor-HAT-Python-Library
+sudo apt-get update
+sudo apt-get install build-essential python-dev python-pip
+sudo python setup.py install
+```
+
+## install AD hat library
+```
+# setup C library
+sudo apt-get install automake libtool
+wget http://www.airspayce.com/mikem/bcm2835/bcm2835-1.50.tar.gz
+tar zxvf bcm2835-1.50.tar.gz
+cd bcm2835-1.50
+autoreconf -vfi
+./configure
+make
+sudo make check
+sudo make install
+
+# setup python library
+sudo apt-get install git build-essential python-dev
+cd ~
+git clone https://github.com/fabiovix/py-ads1256.git
+cd py-ads1256
+sudo python setup.py install
+```
--- temperatureStore.py
+++ temperatureStore.py
@@ -3,39 +3,43 @@ import ads1256
 import threading
 from timeSeriesStore import TimeSeriesStore
 
+ADS1256_INITIALIZED = False
 
 def init_board(gain=1, sps=25):
-    print "initing board..."
-    ads1256.start(str(gain), str(sps))
-    print "inited board..."
+  print "initing board..."
+  ads1256.start(str(gain), str(sps))
+  print "inited board..."
 
-init_board()
 
 
 class TemperatureStore(TimeSeriesStore):
 
-    RUNNING = False
+  RUNNING = False
 
-    def __init__(self, pin=0, sleep=5, calibrations=((0,0), (1,1))):
-        self.pin = pin
-        self.name = "Pin %d" % pin
-        self.sleep = sleep
-        self.calibrations = calibrations
+  def __init__(self, pin=0, sleep=5, calibrations=((0,0), (1,1))):
+    self.pin = pin
+    self.name = "Pin %d" % pin
+    self.sleep = sleep
+    self.calibrations = calibrations
 
-    def start(self):
-        if self.RUNNING:
-            raise Exception("already running")
-        self.RUNNING = True
-        threading.Thread(target=self.poll_temp, args=()).start()
+		if not ADS1256_INITIALIZED:
+			ADS1256_INITIALIZED = True
+			init_board()
+
+  def start(self):
+    if self.RUNNING:
+      raise Exception("already running")
+    self.RUNNING = True
+    threading.Thread(target=self.poll_temp, args=()).start()
         
-    def stop(self):
-        self.RUNNING = False
+  def stop(self):
+    self.RUNNING = False
 
-    def poll_temp(self):
-        while self.RUNNING:
-            value = ads1256.read_channel(self.pin)
-            self.add_temp(self.name, value)
-            time.sleep(self.sleep)
-            if not self.RUNNING:
-                break
+  def poll_temp(self):
+    while self.RUNNING:
+      value = ads1256.read_channel(self.pin)
+      self.add_temp(self.name, value)
+      time.sleep(self.sleep)
+      if not self.RUNNING:
+        break
  
