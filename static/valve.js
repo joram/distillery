7efca691a1f0c0a49fdecea9b811d7c337a4b2c0
John <john.c.oram@gmail.com>
2018-09-05 05:31:42 +0000

    valve and graph all working perdy like

--- static/valve.js
+++ static/valve.js
@@ -2,22 +2,25 @@
 function updateValve(name){
   current = $("#"+name+"-current");
   target = $("#"+name+"-target");
+  percent = $("#"+name+"-percent-bar");
   state = $("#"+name+"-status");
   form = $("#"+name+"-form");
-
 	$.ajax({
 		type: "GET",
 		dataType: "json",
 		url: "/api/valve/"+name,
 		success: function(data){
       current.text(data.current);
-      target.text(data.target);
-      state.text(data.status);
-      if(data.status != "idle") {
-        setTimeout(function(){
-          updateValve(name)
-        }, 500)
+      if(target.text() != data.target){
+        target.text(data.target);
       }
+      percent.css('width', data.current+'%').attr("aria-valuenow", data.current);
+      state.text(data.status);
+      checkMs = 50;
+      if(data.status == "idle") { checkMs = 1000 };
+      setTimeout(function(){
+        updateValve(name);
+      }, checkMs);
     },
 	});
 }
@@ -37,10 +40,6 @@ function setupValve(name){
      type: "POST",
      url: "/api/valve/"+name,
      data: {"target": targetVal},
-     success: function(data){
-       console.log("success");
-       updateValve(name);
-     },
      dataType: "json",
     });
   }
