044282ea9a7c9a384f918bb906adf0fe59aed8ad
John Oram <john@oram.ca>
2019-06-28 22:19:32 -0700

    connect to current url

--- distillery/src/socketio.js
+++ distillery/src/socketio.js
@@ -1,5 +1,11 @@
 import io from 'socket.io-client';
-let socket = io('ws://192.168.1.10:5000');
+
+let url = window.location.origin;
+if(url === "http://localhost:3000"){url = "ws://localhost:5000"}
+if(url.startsWith("https://")){url = url.replace("https://", "ws://")}
+if(url.startsWith("http://")){url = url.replace("http://", "ws://")}
+console.log(url);
+let socket = io(url);
 
 function log_message(data){
     let {module, variable, value } = data;
